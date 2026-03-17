6346c7b8753c515e20d2052b61314002bef5aecb
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-03-17 08:41:28 +0800

    menambahkan class MesinKopi

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿public class KodePaket
+﻿using System.Globalization;
+
+public class KodePaket
 {
     public string getKodePaket(string namaPaket)
     {
@@ -17,19 +19,125 @@
     }
 };
 
-public class Program
+//public class Program
+//{
+//    public static void  Main(String[] args)
+//    {
+//        KodePaket kodePaket = new KodePaket();
+//        while (true)
+//        {
+//            Console.Write("Nama paket: ");
+//            string namaPaket = Console.ReadLine();
+//            string kode = kodePaket.getKodePaket(namaPaket);
+
+//            Console.WriteLine("Kode Paket: " + kode);
+//            Console.WriteLine();
+//        }
+//    }
+//}
+
+public class MesinKopi
 {
-    public static void  Main(String[] args)
+    enum State { OFF, STANDBY, MAINTENANCE, BREWING };
+    public void stateMesinKopi()
     {
-        KodePaket kodePaket = new KodePaket();
+        State state = State.OFF;
+        Console.WriteLine("State mesin kopi awal: " + state);
+        Console.WriteLine("-------------------------------");
+        string input;
+
         while (true)
         {
-            Console.Write("Nama paket: ");
-            string namaPaket = Console.ReadLine();
-            string kode = kodePaket.getKodePaket(namaPaket);
+            switch (state)
+            {
+                case State.OFF:
+                    Console.WriteLine("LIST OPSI " + state + ":");
+                    Console.WriteLine("1. POWER_ON");
+                    Console.WriteLine();
+                    Console.Write("Pilih: ");
+                    input = Console.ReadLine();
 
-            Console.WriteLine("Kode Paket: " + kode);
-            Console.WriteLine();
+                    if (input == "POWER_ON")
+                    {
+                        Console.WriteLine("Mesin Off berubah menjadi Standby.");
+                        Console.WriteLine("-");
+                        state = State.STANDBY;
+                        break;
+                    }
+                    break;
+                case State.STANDBY:
+                    Console.WriteLine("LIST OPSI " + state + ":");
+                    Console.WriteLine("1. START_BREW\n2. START_MAINTENANCE\n3. POWER_OFF");
+                    Console.WriteLine();
+                    Console.Write("Pilih: ");
+                    input = Console.ReadLine();
+
+                    if (input == "START_BREW")
+                    {
+                        Console.WriteLine("Mesin Standby berubah menjadi Brewing.");
+                        Console.WriteLine("-");
+                        state = State.BREWING;
+                        break;
+                    }
+
+                    if (input == "START_MAINTENANCE")
+                    {
+                        Console.WriteLine("Mesin Standby berubah menjadi Maintenance.");
+                        Console.WriteLine("-");
+                        state = State.MAINTENANCE;
+                        break;
+                    }
+
+                    if (input == "POWER_OFF")
+                    {
+                        Console.WriteLine("Mesin Standby berubah menjadi Off.");
+                        Console.WriteLine("-");
+                        state = State.OFF;
+                        break;
+                    }
+                    break;
+                case State.BREWING:
+                    Console.WriteLine("LIST OPSI " + state + ":");
+                    Console.WriteLine("1. FINISH_BREW");
+                    Console.WriteLine();
+                    Console.Write("Pilih: ");
+                    input = Console.ReadLine();
+
+                    if (input == "FINISH_BREW")
+                    {
+                        Console.WriteLine("Mesin Brew berubah menjadi Standby.");
+                        Console.WriteLine("-");
+                        state = State.STANDBY;
+                        break;
+                    }
+                    break;
+                case State.MAINTENANCE:
+                    Console.WriteLine("LIST OPSI " + state + ":");
+                    Console.WriteLine("1. FINISH_MAINTENANCE");
+                    Console.WriteLine();
+                    Console.Write("Pilih: ");
+                    input = Console.ReadLine();
+
+                    if (input == "FINISH_MAINTENANCE")
+                    {
+                        Console.WriteLine("Mesin Maintenance berubah menjadi Standby.");
+                        Console.WriteLine("-");
+                        state = State.STANDBY;
+                        break;
+                    }
+                    break;
+            }
         }
+    }
+}
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        MesinKopi mesinKopi = new MesinKopi();
+            mesinKopi.stateMesinKopi();
+        
+
     }
 }
