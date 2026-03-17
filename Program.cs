76667c82f78cd6df0028a2235b5fb84c0223ed04
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-03-17 08:08:58 +0800

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class KodePaket
+{
+    public string getKodePaket(string namaPaket)
+    {
+        string[] namaPaketArr = { "Basic", "Standard", "Premium", "Unlimited", "Gaming", "Streaming", "Family", "Business", "Student", "Traveler" };
+
+        string[] kodePaket = { "P201", "P202", "P203", "P204", "P205", "P206", "P207", "P208", "P209", "P210" };
+
+        for (int i = 0; i < namaPaketArr.Length; i++)
+        {
+            if (namaPaketArr[i] == namaPaket)
+            {
+                return kodePaket[i];
+            }
+        }
+        return "Kode paket tidak ditemukan";
+    }
+};
+
+public class Program
+{
+    public static void  Main(String[] args)
+    {
+        KodePaket kodePaket = new KodePaket();
+        while (true)
+        {
+            Console.Write("Nama paket: ");
+            string namaPaket = Console.ReadLine();
+            string kode = kodePaket.getKodePaket(namaPaket);
+
+            Console.WriteLine("Kode Paket: " + kode);
+            Console.WriteLine();
+        }
+    }
+}
--- obj/Debug/net10.0/Modul4_103022400090.AssemblyInfo.cs
+++ obj/Debug/net10.0/Modul4_103022400090.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul4_103022400090")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2acc76b11ffb2681c207bcbe7c940ba06d1e3965")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul4_103022400090")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul4_103022400090")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
